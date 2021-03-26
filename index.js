f33de4feb4ca2cfb7b9b2e92b8e6679c20f90b46
Nick Stebb <nickolarse@riseup.net>
2021-03-26 19:59:54 +1300

    Add multer parser

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const app = require('express')()
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser')
 const multer = require('multer') // v1.0.5
 const upload = multer() // for parsing multipart/form-data
+const port = 3000;
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/profile', upload.array(), function (req, res, next) {
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', upload.array(), function (req, res, next) {
   console.log(req.body)
   res.json(req.body)
 })
