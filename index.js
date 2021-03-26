76a27d249ade25b3c45ba20800f351a8284e1281
Nick Stebb <nickolarse@riseup.net>
2021-03-26 19:52:08 +1300

    Add index

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/profile', upload.array(), function (req, res, next) {
+  console.log(req.body)
+  res.json(req.body)
+})
